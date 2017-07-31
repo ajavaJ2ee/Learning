6f55931097052f8d383de31ffd228bd4df5f254e
abhi <abhinai.java@gmail.com>
2017-07-31 18:46:50 -0500

    2nd comment added

--- HelloWorld/src/com/abhinai/firstpackage/HelloWorld.java
+++ HelloWorld/src/com/abhinai/firstpackage/HelloWorld.java
@@ -2,6 +2,9 @@ package com.abhinai.firstpackage;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		//a comment
+		//2nd comment
+		
 		System.out.println("Hello world");
 	}
 
