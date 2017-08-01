87af6bf57ee0df1466f2075b2074cd99e0534a4f
abhi <abhinai.java@gmail.com>
2017-07-31 23:21:56 -0500

    1st Spring project

---
+++ HelloSpring/src/com/abhinai/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.abhinai;
+
+public class HelloWorld {
+
+	public String Mymessage;
+
+	public void getMymessage() {
+		System.out.println("your message"+Mymessage); 
+	}
+
+	public void setMymessage(String message) {
+		this.Mymessage = message;
+	}
+	
+	
+}
